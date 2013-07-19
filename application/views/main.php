c32e9d12649a7685473e13d47cadef5d2ee7003f
Joseph <joseph.dillon.522@gmail.com>
2013-07-19 08:04:28 -0700

    Added Database to repo files

--- application/views/main.php
+++ application/views/main.php
@@ -1,6 +1,7 @@
 <?php
-
+echo "<pre>";
 var_dump($this->session->userdata);
+echo "</pre>";
 echo "<br>";
 
 echo "welcome to main.php";
---
+++ assets/codeigniter_db/database.sql
@@ -0,0 +1,23 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+CREATE SCHEMA IF NOT EXISTS `codeigniter_db` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `codeigniter_db` ;
+
+-- -----------------------------------------------------
+-- Table `codeigniter_db`.`users`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `codeigniter_db`.`users` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `name` VARCHAR(45) NULL ,
+  `password` VARCHAR(200) NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB;
+
+USE `codeigniter_db` ;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
